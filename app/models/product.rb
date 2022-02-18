2029914c087edc78fd4565fe478d253e7ae1d4da
kelecho <kele90@gmail.com>
2022-02-17 22:35:32 -0300

    Controller Products, view, validations and category references

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,6 +8,14 @@
 #  price       :decimal(10, 2)
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  category_id :bigint           not null
 #
 class Product < ApplicationRecord
+
+    validates :name, presence: { message: "O nome do produto é obrigatório!" }
+    validates :description, presence: { message: "A descrição é  obrigatória!" }
+    validates :price, presence: { message: "O preço é obrigatório!" }
+    validates :category_id, presence: { message: "A categoria é obrigatória!" }
+
+    belongs_to :category
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   resources :categories
+  resources :products
   root to: 'home#index'
 end
