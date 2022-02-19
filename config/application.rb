d6a2fcd60bb7e0d9198af5a8c787ddd2ff4f3b84
kelecho <kele90@gmail.com>
2022-02-19 01:01:24 -0300

    pt-BR Devise traduction

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module QuickOrders
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :'pt-BR'
 
     # Don't generate system test files.
     config.generators.system_tests = nil
