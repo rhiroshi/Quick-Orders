76576a270c8662bf189943734bb6461830bb91fe
Rafael H.F.S <rhiroshi14@hotmail.com>
2022-02-20 10:23:31 -0300

    Add kitchen controller, view order list and report order status (#39)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'kitchen/index'
+  get 'kitchen/view_order/:id', to: 'kitchen#view_order', as: 'kitchen_view_order'
+  post 'kitchen/report_status/:id', to: 'kitchen#report_status', as: 'kitchen_report_status'
   resources :orders
   devise_for :users
   resources :categories
