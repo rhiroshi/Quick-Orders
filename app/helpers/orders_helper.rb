92d3c553264324f6ef6b9cdaaf56f88609976c2a
Rafael Shintani <rhiroshi14@hotmail.com>
2022-02-20 07:31:24 -0300

    Add I18n to order.index

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,9 @@
 module OrdersHelper
+  def status_klass(status)
+    return 'bg-primary' if status == 'in_line'
+    return 'bg-info' if status == 'preparing'
+    return 'bg-success' if status == 'ready'
+    return 'bg-secondary' if status == 'finished'
+    return 'bg-secondary' if status == 'canceled'
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -5,7 +5,11 @@ class Order < ApplicationRecord
   validates :table, presence: { message: "É obrigatório mencionar a mesa do pedido!" },
                     numericality: { only_integer: true }
 
-  enum status: { in_line: 0, preparing: 1, ready: 2, canceled: 3 }
+  enum status: { in_line: 0, preparing: 1, ready: 2, canceled: 3, finished: 4 }
 
   belongs_to :user
+
+  def status_name
+    return I18n.t("activerecord.attributes.order.status_name.#{status}")
+  end
 end
