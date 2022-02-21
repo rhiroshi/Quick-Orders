03e6966752f278b061ee44c902541ea4fba3b6aa
kelecho <kele90@gmail.com>
2022-02-21 15:26:01 -0300

    Navbar Link active for navegationa

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def search(&block)
     render('layouts/search', content: capture(&block))
   end
+
+  def active_link controller
+     'active' if controller_name == controller    
+  end
 end
