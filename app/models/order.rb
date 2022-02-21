94316a535c4ca9b1fe584b7c4e4d362dfa6538dc
Gregory <gregory280@hotmail.com>
2022-02-21 01:57:57 -0300

    Order Model Tests

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,13 +1,13 @@
 class Order < ApplicationRecord
-  has_many :order_products, inverse_of: :order
-  accepts_nested_attributes_for :order_products, allow_destroy: true
-  validates :client_name, presence: { message: "O nome do cliente é obrigatório!" }
-  validates :table, presence: { message: "É obrigatório mencionar a mesa do pedido!" },
+  validates :client_name, presence: true, length: { maximum: 40 }
+  validates :table, presence: true,
                     numericality: { only_integer: true }
-
   enum status: { in_line: 0, preparing: 1, ready: 2, canceled: 3, finished: 4 }
+  validates :status, presence: true
 
   belongs_to :user
+  has_many :order_products, inverse_of: :order
+  accepts_nested_attributes_for :order_products, allow_destroy: true
 
   def status_name
     return I18n.t("activerecord.attributes.order.status_name.#{status}")
