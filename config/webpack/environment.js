2da295be5a0817f4140e5937b4203017f4844f1b
Rafael Shintani <rhiroshi14@hotmail.com>
2022-02-21 18:49:57 -0300

    Installed pwa

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,8 +3,10 @@ const { environment } = require('@rails/webpacker')
 const webpack = require('webpack');
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
-  jQuery: 'jquery' 
+  jQuery: 'jquery'
 }));
 
+const WebpackerPwa = require('webpacker-pwa');
+new WebpackerPwa(config, environment);
 
 module.exports = environment
