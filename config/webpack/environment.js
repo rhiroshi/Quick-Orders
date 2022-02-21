44a7213fc6d8adc280db83c921a41e6135cda509
Rafael Shintani <rhiroshi14@hotmail.com>
2022-02-21 18:54:54 -0300

    Revert "Installed pwa"
    This reverts commit 2da295be5a0817f4140e5937b4203017f4844f1b.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,10 +3,8 @@ const { config, environment } = require('@rails/webpacker')
 const webpack = require('webpack');
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
-  jQuery: 'jquery'
+  jQuery: 'jquery' 
 }));
 
-const WebpackerPwa = require('webpacker-pwa');
-new WebpackerPwa(config, environment);
 
 module.exports = environment
